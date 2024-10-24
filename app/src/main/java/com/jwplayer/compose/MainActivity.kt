a75f00cfa881087f16b33e56272798ea5a23b064
amgdeveloper <adria.moreno@dowjones.com>
2024-10-24 11:16:50 +0200

    Add basic project

---
+++ app/src/main/java/com/jwplayer/compose/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.jwplayer.compose
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.jwplayer.compose.ui.jw.Video
+import com.jwplayer.compose.ui.theme.JWComposeTheme
+import com.jwplayer.pub.api.license.LicenseUtil
+
+@ExperimentalAnimationApi
+@ExperimentalComposeUiApi
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    LicenseUtil().setLicenseKey(this, "")
+
+    setContent {
+      JWComposeTheme {
+        val navController = rememberNavController()
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+          NavHost(navController = navController, startDestination = "home") {
+            composable("home") { HomeScreen(navController) }
+            composable("video") { Video(lifecycleOwner = this@MainActivity) }
+          }
+        }
+      }
+    }
+  }
+}
+
+@Composable
+fun HomeScreen(navController: NavController) {
+  Box(
+    modifier = Modifier
+      .fillMaxSize()
+      .padding(16.dp),
+    contentAlignment = Alignment.Center
+  ) {
+    Button(
+      onClick = { navController.navigate("video") },
+      modifier = Modifier.size(100.dp, 50.dp) // Adjust the width and height as needed
+    ) {
+      Text("Show Video")
+    }
+  }
+
+
+
+  DisposableEffect(Unit) {
+    onDispose {
+      Log.e("TAG","HomeScreen onDispose")
+    }
+  }
+}
+
+@Composable
+fun Greeting(name: String) {
+  Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+  JWComposeTheme {
+    Greeting("Android")
+  }
+}
---
+++ app/src/main/java/com/jwplayer/compose/ui/jw/Video.kt
@@ -0,0 +1,36 @@
+package com.jwplayer.compose.ui.jw
+
+import android.util.Log
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.jwplayer.compose.viewmodel.VideoViewModel
+
+@ExperimentalComposeUiApi
+@ExperimentalAnimationApi
+@Composable
+fun Video(
+  lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current
+) {
+  val viewModel: VideoViewModel = viewModel()
+
+  VideoPlayer(
+    modifier = Modifier.fillMaxSize(),
+    videoState = viewModel.uiState.collectAsState().value,
+    lifecycleOwner = lifecycleOwner,
+    handleEvent = viewModel::handleEvent
+  )
+
+  DisposableEffect(Unit) {
+    onDispose {
+      Log.e("TAG","Video Composable onDispose")
+    }
+  }
+}
